ae799909af6c11ec8b4fe540c3d7b6d92350248b
jeroenvervaeck <jeroverv@student.arteveldehs.be>
2018-10-20 14:33:27 +0200

    opdracht 2

--- opdrachten/opdracht2/Program.cs
+++ opdrachten/opdracht2/Program.cs
@@ -7,11 +7,57 @@ namespace opdracht2
         static void Main(string[] args)
         {
             //oef FACULTEIT
-            
+            int n_fac = 10;
+
+            for (int i_fac = 0; i_fac < n_fac; i_fac++)  
+            {  
+                Console.Write( faculteit(i_fac) +" ");  
+            } 
+          
+            Console.Write("\n");
 
             //oef RIJ VAN FIBONACCI
+            int n_fibo = 20;
+
+            for (int i_fibo = 0; i_fibo < n_fibo; i_fibo++)  
+            {  
+                Console.Write( fibonachi(i_fibo) +" ");  
+            } 
+
+            Console.Write("\n");
 
 
         }
+
+    static int faculteit (int facu)
+    {
+        int result = facu;
+        for(int i = facu-1; i > 1; i--)
+        {
+           result = result * i;
+        }
+
+        return result;
     }
+
+
+    static int fibonachi (int n_fibo)
+    {
+
+        int firstnumber = 0;
+        int secondnumber = 1;
+        int result = 0;
+
+        for(int i_fibo = 1; i_fibo <= n_fibo; i_fibo++)
+        {
+            result = firstnumber + secondnumber;
+            firstnumber = secondnumber;
+            secondnumber = result;
+        }
+
+        return result;
+    }
+
+    }
+
 }
